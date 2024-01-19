95da737d7efd2209bcaf358916db12d6936d4adb
Chanuka Hasaranga <96354974+ChanukaHasaranga@users.noreply.github.com>
2024-01-19 15:46:49 +0530

    add_Icon

--- lib/spashscreen1.dart
+++ lib/spashscreen1.dart
@@ -5,35 +5,37 @@ class splashscreen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-decoration: BoxDecoration(
-
-image:DecorationImage(image:AssetImage("assetss/splashscreen2.png"),fit:BoxFit.cover)
-
-
-
-),
-
-child: Column(
- 
-children: [
-Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 130,vertical: 54),
-  child:   Image.asset("assetss/logo2.png",width: 129,height: 129,),
-)
-
-
-],
-
-
-
-
-),
-
-
-
-
-
+    return SafeArea(
+      child: Container(
+    decoration: BoxDecoration(
+    
+    image:DecorationImage(image:AssetImage("assetss/splashscreen2.png"),fit:BoxFit.cover)
+    
+    
+    
+    ),
+    
+    child: Column(
+     
+    children: [
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 130,vertical: 54),
+      child:   Image.asset("assetss/logo2.png",width: 129,height: 129,),
+    )
+    
+    
+    ],
+    
+    
+    
+    
+    ),
+    
+    
+    
+    
+    
+      ),
     );
   }
 }
