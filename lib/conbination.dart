fcc505586b9dd9511892dda23c3d0d8c46d3f2ed
Chanuka Hasaranga <96354974+ChanukaHasaranga@users.noreply.github.com>
2024-01-23 00:27:18 +0530

    combinationcomplete

--- lib/conbination.dart
+++ lib/conbination.dart
@@ -31,15 +31,20 @@ body: SafeArea(
        ),
        Padding(
          padding: const EdgeInsets.fromLTRB(18, 40, 18, 0),
-         child: Container(
-          height: 234,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(image:AssetImage("assetss/GettyImages.png"),fit: BoxFit.cover),
-            borderRadius: BorderRadius.circular(13),
-            border: Border.all(color: Colors.white,width: 1.3),
-
-          ),
+         child: GestureDetector(
+          onTap: () {
+            
+          },
+           child: Container(
+            height: 234,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(image:AssetImage("assetss/GettyImages.png"),fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(13),
+              border: Border.all(color: Colors.white,width: 1.3),
+         
+            ),
+           ),
          ),
        ),
         Positioned(
@@ -59,7 +64,46 @@ body: SafeArea(
         ),
 
 
-      bottomnavigator()
+
+        Padding(
+         padding: const EdgeInsets.fromLTRB(18, 340, 18, 0),
+         child: GestureDetector(
+          onTap: () {
+            
+          },
+           child: Container(
+            height: 234,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(image:AssetImage("assetss/2.png"),fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(13),
+              border: Border.all(color: Colors.white,width: 1.3),
+         
+            ),
+           ),
+         ),
+       ),
+        Positioned(
+          right: 68,
+          top: 550,
+          child: Container(
+            child: Center(child: Text("Non Venomous Snakes",style: TextStyle(fontSize: 20,color: Colors.white,fontFamily: "Poppins"),)),
+        height: 45,
+        width: 261,
+        decoration: BoxDecoration(
+
+          border: Border.all(color:Colors.white,width: 1.3),
+          borderRadius: BorderRadius.circular(23),
+          color: const Color.fromARGB(255, 23, 67, 24)
+        ),
+          ),
+        ),
+
+
+
+        
+
+
 
     ],
 
